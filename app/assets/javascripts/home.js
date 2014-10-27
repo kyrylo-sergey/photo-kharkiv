aa1d45321e23ecb5ba7cdb7508d01c1411302606
Kyrylo Silin <kyrylosilin@gmail.com>
2014-10-27 02:47:43 +0200

    Fix the error when #map doesn't exist

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,16 @@
 /*jslint browser: true*/ /*global  $,L*/
 $(function(){
-  var map = L.map('map');
-  map.setView([50.0139, 36.2253], 14);
-  L.tileLayer('http://tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: 'All rights reserved',
-    maxZoom: 18,
-    subdomains: ''
-  }).addTo(map);
+  var map;
 
+  if ($('#map').length) {
+    map = L.map('map');
+    map.setView([50.0139, 36.2253], 14);
+    L.tileLayer('http://tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'All rights reserved',
+      maxZoom: 18,
+      subdomains: ''
+    }).addTo(map);
 
-  $(window).resize(map.invalidateSize);
+    $(window).resize(map.invalidateSize);
+  }
 });
