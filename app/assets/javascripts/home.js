46cf0e2dca7f66170f75cf5071a6910fe01b2aa0
Kyrylo Silin <kyrylosilin@gmail.com>
2014-10-27 02:35:22 +0200

    Add nice & resizable map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,17 +1,13 @@
+/*jslint browser: true*/ /*global  $,L*/
 $(function(){
-    function resize_map(map){
-	var cont = $(map.getContainer());
-	cont.width($(document).width());
-	cont.height($(document).height()-100); // TODO: unhardcode this
-	map.invalidateSize();
-    }
+  var map = L.map('map');
+  map.setView([50.0139, 36.2253], 14);
+  L.tileLayer('http://tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'All rights reserved',
+    maxZoom: 18,
+    subdomains: ''
+  }).addTo(map);
 
-    var map = L.map('map');
-    map.setView([50.0139, 36.2253], 14);
-    L.tileLayer('http://tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: 'All rights reserved',
-        maxZoom: 18,
-	subdomains: '',
-    }).addTo(map);
-    resize_map(map);
+
+  $(window).resize(map.invalidateSize);
 });
