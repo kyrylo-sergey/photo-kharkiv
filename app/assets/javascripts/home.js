30b1f610f85b505ee79031cd49d1caf6bc1fa6c7
Serhii Pererva <sergey.pererva@gmail.com>
2014-11-01 21:52:55 +0200

    coordinate fileds is populated when adding a new photo, marker is set on map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,12 @@
 /*jslint browser: true*/ /*global  $,L*/
 $(function(){
-  var map, sidebar;
+
+  var map, sidebar, edit_mode_marker;
+
+  function is_edit_mode(){
+    return !$('#sidebar').hasClass('collapsed')
+      && $('#sidebar').find('#profile').hasClass('active');
+  }
 
   if ($('#map').length) {
     map = L.map('map');
@@ -16,6 +22,19 @@ $(function(){
     if ($('#sidebar').length) {
       sidebar = L.control.sidebar('sidebar').addTo(map);
     }
+
+    map.on('click', function(e){
+      if(is_edit_mode()) {
+	if(edit_mode_marker) {
+	  edit_mode_marker.setLatLng(e.latlng)
+	} else {
+	  edit_mode_marker = L.marker(e.latlng);
+	  edit_mode_marker.addTo(map)
+	}
+	$('#photo_latitude').val(e.latlng.lat);
+	$('#photo_longitude').val(e.latlng.lng);
+      }
+    });
   }
 
   $('#markers').data().markers.forEach(function(marker) {
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,10 @@
 class PhotosController < ApplicationController
   def upload
-    photo = current_user.photos.build(params[:photo].permit(:image))
-
+    photo_attrs = params[:photo].permit(:image, :latitude, :longitude)
+    photo = current_user.photos.build(photo_attrs)
     respond_to do |format|
       if photo.save
-        format.html { redirect_to root_url, notice: 'Product was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Photo was successfully created.' }
       else
         format.html { redirect_to root_url, notice: 'Error!' }
       end
