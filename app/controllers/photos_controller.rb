8c56616d0e3602e6216e7f4af6778b9add204ded
Kyrylo Silin <kyrylosilin@gmail.com>
2014-11-01 20:30:49 +0200

    Handle uploads properly

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,13 @@
 class PhotosController < ApplicationController
   def upload
-    photo_attrs = params[:photo].permit(:image)
-    photo = current_user.photos.build(photo_attrs)
-    photo.save!
-    redirect_to root_url
+    photo = current_user.photos.build(params[:photo].permit(:image))
+
+    respond_to do |format|
+      if photo.save
+        format.html { redirect_to root_url, notice: 'Product was successfully created.' }
+      else
+        format.html { redirect_to root_url, notice: 'Error!' }
+      end
+    end
   end
 end
