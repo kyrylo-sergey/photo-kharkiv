b566cd0037d13f0218cc6de815e640c9bb699d23
Serhii Pererva <sergey.pererva@gmail.com>
2014-11-02 00:04:50 +0200

    Photo entity validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,12 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, PhotoUploader
+
+  validates :latitude, :longitude, :image, presence: true
+  validates :latitude, numericality: {
+              greater_than_or_equal_to: -90,
+              less_than_or_equal_to: 90 }
+  validates :longitude, numericality: {
+              greater_than_or_equal_to: -180,
+              less_than_or_equal_to: 180 }
 end
