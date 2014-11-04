e39b215e521cc4070fd97d5b509314d12ef9c339
Serhii Pererva <sergey.pererva@gmail.com>
2014-11-04 22:58:57 +0200

    Better naming

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,8 +2,8 @@
 /*jslint browser: true*/
 require(['domReady'], function (domReady) {
   domReady(function() {
-    require(['./src/app'], function(App) {
-      App.init();
+    require(['./src/map'], function(Map) {
+      Map.init({lat: 50.0139, lng: 36.2253, zoomLvl: 14});
     });
   });
 });
